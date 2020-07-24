70975802d4d29c70770d6436647bc6b95652f7a7
KvngColl <46813694+KvngColl@users.noreply.github.com>
2020-07-24 21:57:34 +0000

    Update Assignment_2_Q2.py

--- Assignment_2_Q2.py
+++ Assignment_2_Q2.py
@@ -33,20 +33,55 @@ def Binary_Search(arr, target):
 
 
 def main():
-  sequence = n.random.randint(1,32767, 5000)
-  start_time = time.time()
-  InterpolationSearch(sequence, 6)
-  end_time = time.time()
 
-  start_time1 = time.time()
-  BinarySearch(sequence, 6)
-  end_time1 = time.time()
+  # for 100
+  sequence = n.random.randint(1,32767, 100)
+  start_time = time.clock()
+  Interpolation_Search(sequence, 6)
+  end_time = time.clock()
+
+  start_time1 = time.clock()
+  Binary_Search(sequence, 6)
+  end_time1 = time.clock()
 
   milsec = (end_time - start_time) * 1000
-  print("The time taken for interpolation search is %.12f" % milsec)
+  print("The time taken for interpolation search using 100 is %.12f" % milsec)
 
   milsec1 = (end_time1 - start_time1) * 1000
-  print("The time taken for binary search is %.12f" % milsec1)
+  print("The time taken for binary search is using 100 %.12f" % milsec1)
+  
+  # for 1000
+  sequence = n.random.randint(1,32767, 1000)
+  start_time = time.clock()
+  Interpolation_Search(sequence, 6)
+  end_time = time.clock()
+
+  start_time1 = time.clock()
+  Binary_Search(sequence, 6)
+  end_time1 = time.clock()
+
+  milsec = (end_time - start_time) * 1000
+  print("The time taken for interpolation search using 1000 is %.12f" % milsec)
+
+  milsec1 = (end_time1 - start_time1) * 1000
+  print("The time taken for binary search using 1000 is %.12f" % milsec1)
+
+  # for 5000
+  
+  sequence = n.random.randint(1,32767, 5000)
+  start_time = time.clock()
+  Interpolation_Search(sequence, 6)
+  end_time = time.clock()
+
+  start_time1 = time.clock()
+  Binary_Search(sequence, 6)
+  end_time1 = time.clock()
+
+  milsec = (end_time - start_time) * 1000
+  print("The time taken for interpolation search using 5000 is %.12f" % milsec)
+
+  milsec1 = (end_time1 - start_time1) * 1000
+  print("The time taken for binary search using 5000 is %.12f" % milsec1)
 
 if __name__ == "__main__":
     main()
